d03a9a5ad72a6eae1d0662e73639e14921e4578c
nikitaparmar04 <nikitaparmar23456@gmail.com>
2024-01-26 22:02:30 +0530

    master

--- memoy game/script.js
+++ memoy game/script.js
@@ -1,6 +1,19 @@
-document.getElementById('checkbox').addEventListener('change',function(){
-    if (this.checked) {
-        // Assuming you want to redirect to 'main_game.html'
+`use strict`
+
+var username = document.getElementById('username');
+
+document.getElementById('checkbox').addEventListener('change', function() {
+        // Get the user's name, selected difficulty level, and selected icon
+        var inputValue = document.getElementById('username').value;
+        console.log(inputValue);
+        username.textContent = inputValue;
+        console.log(username.textContent);
+        var difficultyLevel = document.querySelector('input[name="ageGroup"]:checked').value;
+        var icon = document.querySelector('input[name="ageGroup"]:checked').nextElementSibling.src;
+
         window.location.href = 'PlayArea.html';
-    }
 });
+
+
+
+
