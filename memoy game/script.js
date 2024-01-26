8dd27ccd05efa944a2f90124b8740c7819caa786
nikita parmar <98653385+Nikitaparmar04@users.noreply.github.com>
2024-01-26 16:57:14 +0530

    Add files via upload

---
+++ memoy game/script.js
@@ -0,0 +1,6 @@
+document.getElementById('checkbox').addEventListener('change',function(){
+    if (this.checked) {
+        // Assuming you want to redirect to 'main_game.html'
+        window.location.href = 'testing.html';
+    }
+});
